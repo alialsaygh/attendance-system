42df1171991e7cd8213b709f5771660002241f8c
USER <A.Alsaegh6343@student.leedsbeckett.ac.uk>
2026-04-29 16:14:42 +0100

    Update API endpoint, modify student dashboard layout, and add new student photos

--- pi_reader/scan.py
+++ pi_reader/scan.py
@@ -3,7 +3,7 @@ import requests
 from mfrc522 import SimpleMFRC522
 from verify_face import verify_student_face
 
-FLASK_API = "http://10.104.25.40:5000/api"
+FLASK_API = "http://10.32.13.74:5000/api"
 DEVICE_ID = "pi-rfid-reader-01"
 
 reader = SimpleMFRC522()
